74f46745ccd98ae3888d7a6a971b951956a7bae0
jumper9222 <chindidken@gmail.com>
2025-01-28 09:04:31 +0800

    Added photo_url

--- api/index.js
+++ api/index.js
@@ -56,6 +56,7 @@ app.get("/transactions/:user_id", async (req, res) => {
                 t.user_id AS paid_by,
                 t.category,
                 t.is_split,
+                t.photo_url,
                 s.id AS split_id,
                 s.user_id AS split_uid,
                 s.split_amount, 
@@ -76,6 +77,7 @@ app.get("/transactions/:user_id", async (req, res) => {
                 t.user_id AS paid_by,
                 t.category,
                 t.is_split,
+                t.photo_url,
                 s.id AS split_id,
                 s.user_id AS split_uid,
                 s.split_amount, 
@@ -96,19 +98,19 @@ app.get("/transactions/:user_id", async (req, res) => {
     }
 })
 
-app.get("/comments/:transaction_id", async (req, res) => {
-    const client = await pool.connect();
-    const { transaction_id } = req.params;
-    try {
-        const comments = await client.query("SELECT * FROM comments WHERE transaction_id = $1", [transaction_id])
-        res.status(200).json(comments.rows)
-    } catch (error) {
-        console.error("Error executing query", error.stack)
-        res.status(500).json({ error: "Error fetching comments" })
-    } finally {
-        client.release();
-    }
-})
+// app.get("/comments/:transaction_id", async (req, res) => {
+//     const client = await pool.connect();
+//     const { transaction_id } = req.params;
+//     try {
+//         const comments = await client.query("SELECT * FROM comments WHERE transaction_id = $1", [transaction_id])
+//         res.status(200).json(comments.rows)
+//     } catch (error) {
+//         console.error("Error executing query", error.stack)
+//         res.status(500).json({ error: "Error fetching comments" })
+//     } finally {
+//         client.release();
+//     }
+// })
 
 //Post transaction and post splits conditionally
 app.post("/transaction/:user_id", async (req, res) => {
@@ -121,25 +123,19 @@ app.post("/transaction/:user_id", async (req, res) => {
         currency,
         category,
         isSplit,
+        photoURL,
         splits
     } = req.body;
     const client = await pool.connect();
     try {
         await client.query('BEGIN')
         const transactionResponse = await client.query(
-            `INSERT INTO transactions (title, description, date, total_amount, currency, user_id, category, is_split) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
-            RETURNING 
+            `INSERT INTO transactions (title, description, date, total_amount, currency, user_id, category, is_split, photo_url) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+            RETURNING *,
                 id AS transaction_id,
-                title, 
-                description,
-                date, 
-                total_amount,
-                currency,
-                user_id as paid_by,
-                category, 
-                is_split`,
-            [title, description, date, totalAmount, currency, user_id, category, isSplit]
+                user_id as paid_by`,
+            [title, description, date, totalAmount, currency, user_id, category, isSplit, photoURL]
         );
 
         let splitsResponse = [];
@@ -168,50 +164,50 @@ app.post("/transaction/:user_id", async (req, res) => {
     }
 })
 
-//Post Split
-app.post("/split/:transaction_id", async (req, res) => {
-    const { transaction_id } = req.params;
-    const {
-        user_id,
-        split_amount,
-        currency,
-        category
-    } = req.body;
-    const client = await pool.connect();
-    try {
-        const response = await client.query(
-            "INSERT INTO splits (transaction_id, user_id, split_amount, currency, category) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-            [transaction_id, user_id, split_amount, currency, category]
-        );
-        res.status(201).json(response.rows[0]);
-    } catch (error) {
-        console.error("Error creating split", error)
-        res.status(500).json({ message: "Error creating split", error })
-    } finally {
-        client.release();
-    }
-})
+// //Post Split
+// app.post("/split/:transaction_id", async (req, res) => {
+//     const { transaction_id } = req.params;
+//     const {
+//         user_id,
+//         split_amount,
+//         currency,
+//         category
+//     } = req.body;
+//     const client = await pool.connect();
+//     try {
+//         const response = await client.query(
+//             "INSERT INTO splits (transaction_id, user_id, split_amount, currency, category) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+//             [transaction_id, user_id, split_amount, currency, category]
+//         );
+//         res.status(201).json(response.rows[0]);
+//     } catch (error) {
+//         console.error("Error creating split", error)
+//         res.status(500).json({ message: "Error creating split", error })
+//     } finally {
+//         client.release();
+//     }
+// })
 
 //Post comment
-app.post("/comment/:transaction_id", async (req, res) => {
-    const { transaction_id } = req.params;
-    const {
-        user_id,
-        comment,
-    } = req.body;
-    const client = await pool.connect();
-    try {
-        const response = await client.query(
-            "INSERT INTO comments (transaction_id, user_id, comment) VALUES ($1, $2, $3) RETURNING *",
-            [transaction_id, user_id, comment]
-        );
-        res.status(201).json(response.rows[0]);
-    } catch (error) {
-        res.status(500).send(error)
-    } finally {
-        client.release();
-    }
-})
+// app.post("/comment/:transaction_id", async (req, res) => {
+//     const { transaction_id } = req.params;
+//     const {
+//         user_id,
+//         comment,
+//     } = req.body;
+//     const client = await pool.connect();
+//     try {
+//         const response = await client.query(
+//             "INSERT INTO comments (transaction_id, user_id, comment) VALUES ($1, $2, $3) RETURNING *",
+//             [transaction_id, user_id, comment]
+//         );
+//         res.status(201).json(response.rows[0]);
+//     } catch (error) {
+//         res.status(500).send(error)
+//     } finally {
+//         client.release();
+//     }
+// })
 
 //Update transaction
 app.put("/transaction/:user_id/:transaction_id", async (req, res) => {
@@ -226,6 +222,7 @@ app.put("/transaction/:user_id/:transaction_id", async (req, res) => {
         category,
         paidBy,
         isSplit,
+        photoURL,
         splits
     } = req.body;
     const client = await pool.connect();
@@ -242,9 +239,10 @@ app.put("/transaction/:user_id/:transaction_id", async (req, res) => {
                 date_modified = $6, 
                 category = $7, 
                 user_id = $8, 
-                is_split = $9 
-            WHERE user_id = $10 AND id = $11 RETURNING *`,
-            [title, description, date, totalAmount, currency, dateModified, category, paidBy, isSplit, user_id, transaction_id]
+                is_split = $9,
+                photo_url = $10 
+            WHERE user_id = $11 AND id = $12 RETURNING *`,
+            [title, description, date, totalAmount, currency, dateModified, category, paidBy, isSplit, photoURL, user_id, transaction_id]
         );
         let updatedSplits = []
         if (splits.length > 0) {
@@ -281,69 +279,69 @@ app.put("/transaction/:user_id/:transaction_id", async (req, res) => {
     }
 })
 
-app.put("/transaction/split/:user_id/:transaction_id", async (req, res) => {
-    const { user_id, transaction_id } = req.params;
-    const splits = request.body;
-    const client = pool.connect();
+// app.put("/transaction/split/:user_id/:transaction_id", async (req, res) => {
+//     const { user_id, transaction_id } = req.params;
+//     const splits = request.body;
+//     const client = pool.connect();
 
-    try {
-        const transactionExists = await client.query(
-            'SELECT * FROM transactions WHERE id = $1 AND user_id = $2'
-            [transaction_id, user_id]
-        );
-        const splitsExists = await client.query(
-            'SELECT * FROM splits WHERE transaction_id = $1'
-            [transaction_id]
-        );
+//     try {
+//         const transactionExists = await client.query(
+//             'SELECT * FROM transactions WHERE id = $1 AND user_id = $2'
+//             [transaction_id, user_id]
+//         );
+//         const splitsExists = await client.query(
+//             'SELECT * FROM splits WHERE transaction_id = $1'
+//             [transaction_id]
+//         );
 
-        if (transactionExists.length > 0 && splitsExists.length > 0) {
-            try {
-                const response = await Promise.all(splits.map(async split => {
-                    const { split_amount, currency, category, splitId } = split;
-                    try {
-                        const response = await client.query(
-                            'UPDATE splits SET split_amount = $1, currency = $2, category = $3 WHERE id = $4 RETURNING *',
-                            [split_amount, currency, category, splitId])
-                        console.log("Split updated successfully")
-                        return response.rows[0]
-                    } catch (error) {
-                        console.error("Error updating split", error)
-                        throw error
-                    }
-                }))
-                res.status(201).json(response.rows);
-            } catch (error) {
-                console.error("Error updating splits", error)
-                throw error
-            }
-        }
-    } catch (error) {
-        console.error("Error finding transactions and splits or error updating splits", error)
-    } finally {
-        client.release();
-    }
-})
+//         if (transactionExists.length > 0 && splitsExists.length > 0) {
+//             try {
+//                 const response = await Promise.all(splits.map(async split => {
+//                     const { split_amount, currency, category, splitId } = split;
+//                     try {
+//                         const response = await client.query(
+//                             'UPDATE splits SET split_amount = $1, currency = $2, category = $3 WHERE id = $4 RETURNING *',
+//                             [split_amount, currency, category, splitId])
+//                         console.log("Split updated successfully")
+//                         return response.rows[0]
+//                     } catch (error) {
+//                         console.error("Error updating split", error)
+//                         throw error
+//                     }
+//                 }))
+//                 res.status(201).json(response.rows);
+//             } catch (error) {
+//                 console.error("Error updating splits", error)
+//                 throw error
+//             }
+//         }
+//     } catch (error) {
+//         console.error("Error finding transactions and splits or error updating splits", error)
+//     } finally {
+//         client.release();
+//     }
+// })
 
-app.put("/comment/:transaction_id/:comment_id", async (req, res) => {
-    const { transaction_id, comment_id } = req.params;
-    const {
-        user_id,
-        comment,
-    } = req.body;
-    const client = await pool.connect();
-    try {
-        const response = await client.query(
-            "UPDATE comments SET comment = $1 WHERE transaction_id = $2 AND id = $3 AND user_id = $4 RETURNING *",
-            [comment, transaction_id, comment_id, user_id]
-        );
-        res.status(201).json(response.rows[0]);
-    } catch (error) {
-        console.error("Error updating comment", error)
-        res.status(500).send(error)
-    } finally {
-        client.release();
-    }
-})
+// app.put("/comment/:transaction_id/:comment_id", async (req, res) => {
+//     const { transaction_id, comment_id } = req.params;
+//     const {
+//         user_id,
+//         comment,
+//     } = req.body;
+//     const client = await pool.connect();
+//     try {
+//         const response = await client.query(
+//             "UPDATE comments SET comment = $1 WHERE transaction_id = $2 AND id = $3 AND user_id = $4 RETURNING *",
+//             [comment, transaction_id, comment_id, user_id]
+//         );
+//         res.status(201).json(response.rows[0]);
+//     } catch (error) {
+//         console.error("Error updating comment", error)
+//         res.status(500).send(error)
+//     } finally {
+//         client.release();
+//     }
+// })
 
 //Delete transaction
 app.delete("/transaction/:user_id/:transaction_id", async (req, res) => {
@@ -362,38 +360,38 @@ app.delete("/transaction/:user_id/:transaction_id", async (req, res) => {
     }
 })
 
-app.delete("/split/:user_id/:transaction_id/:split_id", async (req, res) => {
-    const client = await pool.connect();
-    const { user_id, transaction_id, split_id } = req.params;
-    try {
-        const response = await client.query(
-            "DELETE FROM splits WHERE user_id = $1 AND transaction_id = $2 AND id = $3 RETURNING *",
-            [user_id, transaction_id, split_id])
-        res.status(200).json(response.rows)
-    } catch (error) {
-        console.error("Error split", error)
-        res.status(500).send(error);
-    } finally {
-        client.release();
-    }
-})
+// app.delete("/split/:user_id/:transaction_id/:split_id", async (req, res) => {
+//     const client = await pool.connect();
+//     const { user_id, transaction_id, split_id } = req.params;
+//     try {
+//         const response = await client.query(
+//             "DELETE FROM splits WHERE user_id = $1 AND transaction_id = $2 AND id = $3 RETURNING *",
+//             [user_id, transaction_id, split_id])
+//         res.status(200).json(response.rows)
+//     } catch (error) {
+//         console.error("Error split", error)
+//         res.status(500).send(error);
+//     } finally {
+//         client.release();
+//     }
+// })
 
-app.delete("/comment/:user_id/:comment_id", async (req, res) => {
-    const { user_id, comment_id } = req.params;
-    const client = await pool.connect();
-    try {
-        const response = await client.query(
-            "DELETE FROM comments WHERE id = $1 AND user_id = $2 RETURNING *",
-            [comment_id, user_id]
-        );
-        res.status(201).json(response.rows[0]);
-    } catch (error) {
-        console.error("Error deleting comment", error)
-        res.status(500).send(error)
-    } finally {
-        client.release();
-    }
-})
+// app.delete("/comment/:user_id/:comment_id", async (req, res) => {
+//     const { user_id, comment_id } = req.params;
+//     const client = await pool.connect();
+//     try {
+//         const response = await client.query(
+//             "DELETE FROM comments WHERE id = $1 AND user_id = $2 RETURNING *",
+//             [comment_id, user_id]
+//         );
+//         res.status(201).json(response.rows[0]);
+//     } catch (error) {
+//         console.error("Error deleting comment", error)
+//         res.status(500).send(error)
+//     } finally {
+//         client.release();
+//     }
+// })
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
